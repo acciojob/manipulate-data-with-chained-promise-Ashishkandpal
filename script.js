33b1d9ced7979c5be533040cfc589095cc41839b
Ashish Kandpal <63722194+Ashishkandpal@users.noreply.github.com>
2023-05-18 00:10:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+
+const output = document.querySelector("#output");
+const arr = [1, 2, 3, 4];
+let odd, even;
+
+const func = async () => {
+  const first = await new Promise((resolve) => {
+    setTimeout(() => {
+      odd = arr.filter((val) => val % 2 !== 0);
+      console.log(odd);
+      output.innerText = [...odd];
+      resolve(odd);
+    }, 1000);
+  });
+  const second = await new Promise((resolve) => {
+    setTimeout(() => {
+      even = arr.filter((val) => {
+        if (val % 2 === 0) {
+          return val * 2;
+        }
+      });
+      console.log(even);
+      output.innerText = [...even];
+      resolve(even);
+    }, 2000);
+  });
+};
+
+func();
